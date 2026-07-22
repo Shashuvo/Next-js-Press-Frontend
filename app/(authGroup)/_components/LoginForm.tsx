586932fc543f5e961a73de53741defd11467185e
Shashuvo <shahariat2810@gmail.com>
2026-07-22 22:55:03 +0600

    login form action added with backend

--- app/(authGroup)/_components/LoginForm.tsx
+++ app/(authGroup)/_components/LoginForm.tsx
@@ -2,10 +2,11 @@
 
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
+import { loginAction } from "../_actions/authActions"
 
 const LoginForm = () => {
     return (
-        <form className="space-y-4">
+        <form action={loginAction} className="space-y-4">
             <Card className="p-5 space-y-4">
                 <input type="email" name="email" placeholder="Enter Your Email" required />
                 <input type="password" name="password" placeholder="Enter Your Password" required />
