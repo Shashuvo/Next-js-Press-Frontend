f557db18df78b50172fe1f5214c846c5de2faea5
Shashuvo <shahariat2810@gmail.com>
2026-07-27 21:56:44 +0600

    handled auth routes in proxy file

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,40 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import jwt, { JwtPayload } from "jsonwebtoken"
+
+const AUTH_ROUTES = ["/login", "/register"];
 
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
+    const pathName = request.nextUrl.pathname;
+
+    const accessToken = request.cookies.get("accessToken")?.value;
+
+    const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null;
+
+    let userRole = null;
+
+    if (decodedToken) {
+        userRole = decodedToken.role;
+    }
+
+    // user is logged in but trying to access login or register, then redirect to their role dashboard or home page
+    if (accessToken && AUTH_ROUTES.includes(pathName)) {
+        if (userRole === "USER") {
+            return NextResponse.redirect(new URL('/dashboard', request.url));
+        }
+        else if (userRole === "ADMIN") {
+            return NextResponse.redirect(new URL('/admin-dashboard', request.url));
+        }
+        else if (userRole === "AUTHOR") {
+            return NextResponse.redirect(new URL('/author-dashboard', request.url));
+        }
+        else {
+            return NextResponse.redirect(new URL('/', request.url));
+        }
+    }
+
     // return NextResponse.redirect(new URL('/', request.url))
-    console.log(request)
     return NextResponse.next();
 }
 
