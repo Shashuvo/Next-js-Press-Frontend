cca0c915ef94bb8bcbe212e835cb8cd681a785de
Shashuvo <shahariat2810@gmail.com>
2026-08-03 22:11:49 +0600

    all pages added

--- app/(publicGroup)/_components/news/PremiumNewsList.tsx
+++ app/(publicGroup)/_components/news/PremiumNewsList.tsx
@@ -1,12 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
+import { NewsCard } from './NewsCard'
+import { IPost } from '@/lib/types';
 
 const PremiumNewsList = () => {
+
+  
+  const result = {
+    success: true,
+    data: [
+      {
+        id: "2",
+        title: "premium News 1",
+        content: "This is the content of premium news 1.",
+        thumbnail: "https://via.placeholder.com/150",
+        isFeatured: true,
+        status: "PUBLISHED",
+        tags: ["tag1", "tag2"],
+        views: 100,
+        isPremium: true,
+        authorId: "1",
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      }
+    ]
+  };
+
+  if (!result.success || !result.data?.length) {
+    return (
+      <p className="py-12 text-center text-muted-foreground">
+        No premium news found.
+      </p>
+    );
+  }
+
+
   return (
     <div className="space-y-8">
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {/* {result.data.map((post: IPost) => (
+        {result.data.map((post: IPost | any) => (
           <NewsCard key={post.id} post={post} />
-        ))} */}
+        ))}
       </div>
     </div>
   )
