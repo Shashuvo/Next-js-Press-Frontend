38e8839d07ebe1ca3c2ea6899cc01390801a6bf0
Shashuvo <shahariat2810@gmail.com>
2026-08-10 20:06:04 +0600

    edit post modal and functionalities added

--- app/(dashboardGroup)/_components/PostFormDialog.tsx
+++ app/(dashboardGroup)/_components/PostFormDialog.tsx
@@ -11,7 +11,7 @@ import { IPost } from "@/lib/types";
 import { PencilIcon, PlusIcon } from "lucide-react";
 import { useActionState, useEffect, useState } from "react";
 import { toast } from "sonner";
-import { createPost } from "../_actions/myPostsActions";
+import { createPost, updatePost } from "../_actions/myPostsActions";
 // import { createPost, updatePost } from "../_actions/myPostsActions";
 
 type PostFormDialogProps = {
@@ -22,13 +22,10 @@ type PostFormDialogProps = {
 export function PostFormDialog({ mode, post }: PostFormDialogProps) {
     const [open, setOpen] = useState(false);
 
-    // const action = mode === "edit" && post
-    //     ? updatePost.bind(null, post.id)
-    //     : createPost;
-
     const action = mode === "edit" && post
-        ? () => { }
+        ? updatePost.bind(null, post.id)
         : createPost;
+
 
     const [state, formAction, pending] = useActionState(action, null) as any;
 
